35bd1ba94e3a056a20f24c5b72c52a30d76ecb12
Amit Nambiar <amit.lzkpa@gmail.com>
2019-01-16 05:46:15 -0500

    move things around

--- index.js
+++ index.js
@@ -12,45 +12,65 @@ const shell = require('shelljs')
 let repoPath = ''
 repoPath = 'test-repo'
 
-
-
 let branch = 'master'
 
 
 
-shell.cd(repoPath)
+
+
+console.log(`Starting analysis on ${repoPath} at commit ${branch} ...`)
 console.log('----------------')
-let lsTreeReport = shell.exec(`git ls-tree --full-tree -r --name-only ${branch}`, {silent:true})
 
-if (lsTreeReport.code == 0) {
-  let files = lsTreeReport.stdout.split('\n')
-  let fileScores = {}
-  let motherDict = {}
-  for (let i = 0; i < files.length; i++) {
-    let scrDict = countScoresForFile(files[i])
-    fileScores[files[i]] = scrDict
-    for (let s in scrDict) {
-      if (motherDict[s] === undefined) motherDict[s] = 0
-      motherDict[s] += scrDict[s]
-    }
-    console.log(files[i]);
-    console.log(scrDict);
-    console.log('-');
+
+let scores = countScoresForRepo(repoPath, branch)
+
+
+let motherDict = {}
+let i = 0
+for (let file in scores) {
+  let scrDict = scores[file]
+  for (let s in scrDict) {
+    if (motherDict[s] === undefined) motherDict[s] = 0
+    motherDict[s] += scrDict[s]
   }
-
-  console.log('==========');
-  console.log(motherDict);
-
+  console.log(`${i+1}. \t${file}`);
+  console.log(scrDict);
+  i++;
 }
 
+
 console.log('----------------')
+console.log(`Cumulative score`)
+console.log(motherDict);
 
 
 
 
 
-function countScoresForFile(fPath) {
-  let fileBlameReport = shell.exec(`git blame --line-porcelain -w -M -e ${branch} ${fPath}`, {silent:true})
+
+
+
+function countScoresForRepo(rPath, commit='HEAD') {
+  shell.cd(rPath)
+  let lsTreeReport = shell.exec(`git ls-tree --full-tree -r --name-only ${commit}`, {silent:true})
+
+  if (lsTreeReport.code == 0) {
+    let files = lsTreeReport.stdout.split('\n')
+    let fileScores = {}
+    for (let i = 0; i < files.length; i++) {
+      let scrDict = countScoresForFile(files[i], commit)
+      fileScores[files[i]] = scrDict
+    }
+    return fileScores;
+  }
+}
+
+
+
+
+
+function countScoresForFile(fPath, commit='HEAD') {
+  let fileBlameReport = shell.exec(`git blame --line-porcelain -w -M -e ${commit} ${fPath}`, {silent:true})
   if(fileBlameReport.code == 0) {
     let fileBlameRes = fileBlameReport.stdout
     let scoreDict = {}
